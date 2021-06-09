32d3f0b2e03a989b4471e231f984ebd2d906343f
Shawon Saha <shawondroid@gmail.com>
2021-06-09 19:01:46 +0600

    Remove List Items (UseStateArray()) https://imgur.com/YL2SKRf

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  const removeSelected = (id) => {
+    const newPeople = people.filter((p) => p.id !== id);
+    setPeople(newPeople);
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <h2 key={id} className="item">
+            {name}
+            <button onClick={() => removeSelected(id)}>remove</button>
+          </h2>
+        );
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        Clear All
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
