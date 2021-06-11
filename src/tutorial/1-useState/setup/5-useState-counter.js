12bb2c4e94ea316e8223ff996e96736d71df2839
Shawon Saha <shawondroid@gmail.com>
2021-06-11 22:14:29 +0600

    Increase Decrease or Reset mouse click

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [value, setValue] = useState(0);
+  const increase = () => {
+    setValue(value + 1);
+  };
+
+  const decrease = () => {
+    setValue(value - 1);
+  };
+
+  const reset = () => {
+    setValue(0);
+  };
+  return (
+    <>
+      <h1>Click Counter</h1>
+      <h2>{value}</h2>
+      <button className="btn" onClick={() => decrease()}>
+        Decrease
+      </button>
+      <button className="btn" onClick={reset}>
+        Reset
+      </button>
+      <button className="btn" onClick={() => increase()}>
+        Increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateCounter;
